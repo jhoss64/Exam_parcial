d22daac339fa87d51c37c3a3b3e1fc40be5815d9
jhoss64 <jhossmm@gmail.com>
2018-10-27 11:02:46 -0500

    subida2

--- src/pregunta3_bigO/Combinaciones.java
+++ src/pregunta3_bigO/Combinaciones.java
@@ -4,7 +4,33 @@ public class Combinaciones {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Combinaciones combinations = new Combinaciones();
+		int n = 1;
+		System.out.println("# Ladder's steps: " + n);
+		for(int i = 0;i<n;i++) {
+			int level = 0;
+			long a1,a2;
+			a1 = countCombinations(n-i,level+1);
+			a2 = countCombinations(n-i,level+1)+a1;
+			level++;
+			
+		}
+		System.out.println("# Step combinations: " + combinations.countCombinations(n, 0));
+		
+	
+	}
+	public static long countCombinations(int n, int level) {
+		int a = 0;
+		if (n < 0) {
+			a=0;
+		}
+		if (n == 0 && level == 0) {
+			a=0;
+		}
+		if (n == 0 && level > 0) {
+			a=1;
+		}	
+		return a;
 	}
 
 }
